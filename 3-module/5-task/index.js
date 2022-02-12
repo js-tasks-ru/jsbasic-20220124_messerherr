1a0b00afc612cdf62f5f06c0a19d81b1de4fc398
messerherr <messerherr@mail.ru>
2022-02-12 22:28:59 +0300

    3-module_Homework_done_ver.2.__task-5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,10 +1,12 @@
 function getMinMax(str) {
-    let newArray = str.split(" "); //разделям строку
-    let numberArray = newArray.filter(item => isFinite(item)); //для проверки параметра на то, что он является конечным числом
-    let result = { //вывод данных в массив по мин и макс
-        min: Math.min.apply(null, numberArray),
-        max: Math.max.apply(null, numberArray),
+
+    let newArr = str.split(" ");
+
+    let numberArr = newArr.filter(item => isFinite(item));
+
+    let result = {
+        min: Math.min.apply(null, numberArr),
+        max: Math.max.apply(null, numberArr),
     };
     return result;
 }
-console.log(getMinMax(inputData));
