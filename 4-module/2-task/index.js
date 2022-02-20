6fd95cb5e2726f402ab15895d1f0071d095e6e08
messerherr <messerherr@mail.ru>
2022-02-20 22:36:24 +0300

    Homework_module#4_ver.1.0__done

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,12 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+    //Каждая новая окрашенная ячейка я ряду - это +1 по сравнению со старым рядом
+
+    // для решения рекомендуется использовать свойства rows таблицы (элемент <table>) и cells элемента строки (элемент <tr>).
+    for (let i = 0; i < table.rows.length; i++) {
+        let row = table.rows[i];
+
+        //для решения рекомендуется использовать свойства  cells элемента строки (элемент <tr>).
+        row.cells[i].style.backgroundColor = "red";
+    }
+    return table;
 }
