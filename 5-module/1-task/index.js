f3e9cddf2cb636dd7350452b941cb5bcd4b6c906
messerherr <messerherr@mail.ru>
2022-03-26 22:53:24 +0300

    5-module__done

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,11 @@
 function hideSelf() {
-  // ваш код...
+    let button = document.querySelector('.hide-self-button'); // сначала обращаемся  кнужному элементу дерева и выбираем нашу кнопку
+
+    button.onclick = function() {
+            button.hidden = true;
+        }
+        // либо короткий вариант записи через стрелочную функцию
+        //button.onclick = () => {
+        //button.hidden = true;
+
 }
